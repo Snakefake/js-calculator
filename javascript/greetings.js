847417d63b60e887fc5b17e3a83f16efa4c0f5d1
Snakefake <romashkov178@gmail.com>
2022-02-17 12:56:41 +0200

    Update greetings.js

--- javascript/greetings.js
+++ javascript/greetings.js
@@ -2,6 +2,6 @@ var person;
 if(confirm("Are you sure?")) {
         person = prompt("Hi, what's your name?");
         personLastName = prompt("What's your LastName?");
-        alert("Hello, " + person + personLastName);
+        alert("Hello, " + person + personLastName + "!" + "How Are You?");
     } else
         alert("Person not sure");
